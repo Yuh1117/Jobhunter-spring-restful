56c5d667aac4785a75d1d1c4e7e058e0b0c0b07d
Yuh <huypham3188@gmail.com>
2024-11-13 21:56:59 +0700

    create token

--- src/main/java/vn/vpgh/jobhunter/controller/AuthController.java
+++ src/main/java/vn/vpgh/jobhunter/controller/AuthController.java
@@ -10,21 +10,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import vn.vpgh.jobhunter.domain.dto.LoginDTO;
+import vn.vpgh.jobhunter.domain.dto.ResLoginDTO;
+import vn.vpgh.jobhunter.util.SecurityUtil;
 
 @RestController
 public class AuthController {
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final SecurityUtil securityUtil;
 
-    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder) {
+    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil) {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
+        this.securityUtil = securityUtil;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginDTO> login(@Valid @RequestBody LoginDTO login) {
+    public ResponseEntity<ResLoginDTO> login(@Valid @RequestBody LoginDTO login) {
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                 login.getUsername(), login.getPassword());
 
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        return ResponseEntity.status(HttpStatus.OK).body(login);
+
+        ResLoginDTO resLoginDTO = new ResLoginDTO();
+        resLoginDTO.setAccessToken(this.securityUtil.createToken(authentication));
+
+        return ResponseEntity.status(HttpStatus.OK).body(resLoginDTO);
     }
 }

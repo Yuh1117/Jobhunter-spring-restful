0868cb6a4a96f6a0533637343480d5289657b5ea
Yuh <huypham3188@gmail.com>
2025-02-06 17:05:29 +0700

    update security & update filter resume

--- src/main/java/vn/vpgh/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/vpgh/jobhunter/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import com.nimbusds.jose.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -38,16 +39,18 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http,
-            CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
-        String[] whiteList = { "/", "/api/v0.1/auth/login", "/api/v0.1/auth/register", "/api/v0.1/auth/refresh",
-                "/storage/**", "/api/v0.1/companies/**", "/api/v0.1/jobs/**" };
+                                           CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
+        String[] whiteList = {"/", "/api/v0.1/auth/login", "/api/v0.1/auth/register", "/api/v0.1/auth/refresh",
+                "/storage/**", "/api/v0.1/companies/**", "/api/v0.1/jobs/**"};
         http
                 .csrf(c -> c.disable())
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(
                         authz -> authz
-                                .requestMatchers(whiteList)
-                                .permitAll()
+                                .requestMatchers(whiteList).permitAll()
+                                .requestMatchers(HttpMethod.GET, "api/v0.1/companies").permitAll()
+                                .requestMatchers(HttpMethod.GET, "api/v0.1/jobs").permitAll()
+                                .requestMatchers(HttpMethod.GET, "api/v0.1/skills").permitAll()
                                 .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
                         .authenticationEntryPoint(customAuthenticationEntryPoint))
--- src/main/java/vn/vpgh/jobhunter/controller/ResumeController.java
+++ src/main/java/vn/vpgh/jobhunter/controller/ResumeController.java
@@ -1,30 +1,49 @@
 package vn.vpgh.jobhunter.controller;
 
 import com.turkraft.springfilter.boot.Filter;
+import com.turkraft.springfilter.builder.FilterBuilder;
+import com.turkraft.springfilter.converter.FilterSpecificationConverter;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import vn.vpgh.jobhunter.domain.Company;
+import vn.vpgh.jobhunter.domain.Job;
 import vn.vpgh.jobhunter.domain.Resume;
+import vn.vpgh.jobhunter.domain.User;
 import vn.vpgh.jobhunter.domain.response.ResultPaginationDTO;
 import vn.vpgh.jobhunter.domain.response.resume.ResCreateResumeDTO;
 import vn.vpgh.jobhunter.domain.response.resume.ResResumeDTO;
 import vn.vpgh.jobhunter.domain.response.resume.ResUpdateResumeDTO;
 import vn.vpgh.jobhunter.service.ResumeService;
+import vn.vpgh.jobhunter.service.UserService;
+import vn.vpgh.jobhunter.util.SecurityUtil;
 import vn.vpgh.jobhunter.util.annotation.ApiMessage;
 import vn.vpgh.jobhunter.util.error.IdInvalidException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/api/v0.1")
 public class ResumeController {
     private final ResumeService resumeService;
+    private final UserService userService;
 
-    public ResumeController(ResumeService resumeService) {
+    private final FilterSpecificationConverter filterSpecificationConverter;
+    private final FilterBuilder filterBuilder;
+
+    public ResumeController(ResumeService resumeService, UserService userService,
+                            FilterSpecificationConverter filterSpecificationConverter, FilterBuilder filterBuilder) {
         this.resumeService = resumeService;
+        this.userService = userService;
+        this.filterSpecificationConverter = filterSpecificationConverter;
+        this.filterBuilder = filterBuilder;
     }
 
     @PostMapping("/resumes")
@@ -47,8 +66,31 @@ public class ResumeController {
     @GetMapping("/resumes")
     @ApiMessage("Get all resumes")
     public ResponseEntity<ResultPaginationDTO> getAllResumes(@Filter Specification<Resume> specification,
-            Pageable pageable) {
-        return ResponseEntity.ok().body(this.resumeService.getAllResume(specification, pageable));
+                                                             Pageable pageable) {
+        List<Long> jobIds = new ArrayList<>();
+
+        String email = SecurityUtil.getCurrentUserLogin().isPresent() ? SecurityUtil.getCurrentUserLogin().get() : "";
+
+        User currentUser = this.userService.getUserByEmail(email);
+        if (currentUser != null) {
+            Company company = currentUser.getCompany();
+            if (company != null) {
+                List<Job> companyJobs = company.getJobs();
+                if (companyJobs != null && companyJobs.size() > 0) {
+                    jobIds = companyJobs.stream().map(j -> j.getId()).collect(Collectors.toList());
+                }
+            }
+        }
+
+        List<Long> finalJobIds = jobIds;
+        Specification<Resume> joinSpec = (root, query, criteriaBuilder) -> {
+            return criteriaBuilder.in(root.get("job").get("id")).value(finalJobIds);
+        };
+
+        Specification<Resume> finalSpec = joinSpec.and(specification);
+
+
+        return ResponseEntity.ok().body(this.resumeService.getAllResume(finalSpec, pageable));
     }
 
     @PutMapping("/resumes")
